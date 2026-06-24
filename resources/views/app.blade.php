16f02c15bd8c9fdebe69755f017306357ea2e5d9
nagaprasad-ts <tsnagaprasadts@gmail.com>
2026-06-24 11:31:43 +0530

    feat: google tag manager

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,6 +5,14 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
+        <!-- Google Tag Manager -->
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-KS4TVQS6');</script>
+        <!-- End Google Tag Manager -->
+
         {{-- Inline style to set the HTML background color --}}
         <style>
             html {
@@ -23,6 +31,10 @@
         </x-inertia::head>
     </head>
     <body class="font-sans antialiased">
+        <!-- Google Tag Manager (noscript) -->
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KS4TVQS6"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        <!-- End Google Tag Manager (noscript) -->
         <x-inertia::app />
     </body>
 </html>
